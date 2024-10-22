bd6a78d2aaf5ab935b9fbfff20ff9e184d1095a9
yunseohhe <dbstj4566@naver.com>
2024-10-22 14:52:05 +0900

    init

---
+++ src/main/java/com/sparta/doguin/DoguinApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.doguin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoguinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoguinApplication.class, args);
+    }
+
+}
