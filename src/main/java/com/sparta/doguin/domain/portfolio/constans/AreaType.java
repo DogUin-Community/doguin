eb18bcd84fc07985817725893353712590f308fc
kkm4512 <nayoun440@naver.com>
2024-10-22 17:03:11 +0900

    feat. 포트폴리오 엔티티 추가

---
+++ src/main/java/com/sparta/doguin/domain/portfolio/constans/AreaType.java
@@ -0,0 +1,30 @@
+package com.sparta.doguin.domain.portfolio.constans;
+
+import lombok.Getter;
+
+@Getter
+public enum AreaType {
+    SEOUL("서울"),
+    GYEONGGI("경기도"),
+    INCHEON("인천"),
+    BUSAN("부산"),
+    DAEGU("대구"),
+    DAEJEON("대전"),
+    GWANGJU("광주"),
+    ULSAN("울산"),
+    SEJONG("세종"),
+    GANGWON("강원도"),
+    CHUNGCHEONGBUK("충청북도"),
+    CHUNGCHEONGNAM("충청남도"),
+    JEOLLABUK("전라북도"),
+    JEOLLANAM("전라남도"),
+    GYEONGSANGBUK("경상북도"),
+    GYEONGSANGNAM("경상남도"),
+    JEJU("제주도");
+
+    private final String description;
+
+    AreaType(String description) {
+        this.description = description;
+    }
+}
