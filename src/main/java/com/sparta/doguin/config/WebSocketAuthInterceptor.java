073fae22ee6e88a61658b6fba7a56c23f126bff6
chukye1216 <chukye1216@gmail.com>
2024-10-24 11:42:28 +0900

    fix. redis 코드 수정

--- src/main/java/com/sparta/doguin/config/WebSocketAuthInterceptor.java
+++ src/main/java/com/sparta/doguin/config/WebSocketAuthInterceptor.java
@@ -24,7 +24,6 @@ public class WebSocketAuthInterceptor implements ChannelInterceptor {
     public Message<?> preSend(@NonNull Message<?> message,@NonNull MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
 
-        // WebSocket 연결 시 헤더에서 Authorization 가져오기
         String token = accessor.getFirstNativeHeader("Authorization");
 
         if (token != null && token.startsWith("Bearer ")) {
@@ -32,21 +31,17 @@ public class WebSocketAuthInterceptor implements ChannelInterceptor {
                 String jwt = token.substring(7);  // "Bearer " 제거
                 Claims claims = jwtUtil.extractClaims(jwt);
 
-                // JWT에서 사용자 정보 추출
                 Long userId = Long.valueOf(claims.getSubject());
                 String email = claims.get("email", String.class);
                 String nickname = claims.get("nickname", String.class);
                 UserType userType = UserType.of(claims.get("userType", String.class));
                 UserRole userRole = UserRole.of(claims.get("userRole", String.class));
 
-                // AuthUser 객체 생성
                 AuthUser authUser = new AuthUser(userId, email, nickname, userType, userRole);
 
-                // SecurityContextHolder에 인증 정보 저장
                 JwtAuthenticationToken authenticationToken = new JwtAuthenticationToken(authUser);
                 SecurityContextHolder.getContext().setAuthentication(authenticationToken);
 
-                // WebSocket 세션에 사용자 정보를 저장 (필요하면)
                 accessor.setUser(authenticationToken);
             } catch (Exception e) {
                 log.error("JWT 토큰 인증 실패", e);
