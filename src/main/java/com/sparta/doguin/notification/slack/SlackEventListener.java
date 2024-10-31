798cc33443a31ffeef3a11a8bea29758e97b1f04
Changmin Kim <ksjchm4@gmail.com>
2024-10-31 19:51:18 +0900

    feat. 비동기로 처리할 메소드를 별도의 클래스로 분리하고 비동기 구현 완료

--- src/main/java/com/sparta/doguin/notification/slack/SlackEventListener.java
+++ src/main/java/com/sparta/doguin/notification/slack/SlackEventListener.java
@@ -2,15 +2,16 @@ package com.sparta.doguin.notification.slack;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-import com.sparta.doguin.domain.follow.dto.FollowResponse;
 import com.sparta.doguin.domain.follow.service.FollowService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
-import org.springframework.http.*;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -25,7 +26,7 @@ public class SlackEventListener {
     @Value(value = "${slack.bot-token}")
     private String token;
 
-
+    private final SlackSendPush slackSendPush;
     private final FollowService followService;
 
     @EventListener
@@ -51,46 +52,13 @@ public class SlackEventListener {
         for (String email  : emails) {
             String userEmail = email;
             String slackId = findEmail(userEmail);
-            sendPush(slackId, nickName, message);
+            slackSendPush.sendPush(slackId, nickName, message);
         }
 
 
 
     }
 
-    @Async // 비동기
-    protected void sendPush(String slackId, String nickName, String message) {
-        log.info("시작");
-        String url = "https://slack.com/api/chat.postMessage";
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Bearer " + token);
-        headers.add("Content-Type", "application/json; charset=utf-8");
-
-        // JSON 본문 생성
-        JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("channel", slackId);
-        jsonObject.addProperty("text", nickName + " " + message);
-        String body = jsonObject.toString();
-
-        HttpEntity<String> requestEntity = new HttpEntity<>(body, headers);
-        RestTemplate restTemplate = new RestTemplate();
-
-        try {
-            ResponseEntity<String> responseEntity = restTemplate.exchange(
-                    url, HttpMethod.POST, requestEntity, String.class);
-
-            HttpStatus httpStatus = (HttpStatus) responseEntity.getStatusCode();
-            int status = httpStatus.value();
-            String response = responseEntity.getBody();
-
-            System.out.println("Status: " + status);
-            System.out.println("Response: " + response);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        log.info("끝");
-    }
 
     protected String findEmail(String email) {
         String url = "https://slack.com/api/users.lookupByEmail?email=" + email;
