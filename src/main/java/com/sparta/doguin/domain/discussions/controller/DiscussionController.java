e2c0f5283a0b52255dc967de24b2c57290834d05
chukye1216 <chukye1216@gmail.com>
2024-11-07 17:56:29 +0900

    fix. 토론게시판 컨트롤러 코드 수정

--- src/main/java/com/sparta/doguin/domain/discussions/controller/DiscussionController.java
+++ src/main/java/com/sparta/doguin/domain/discussions/controller/DiscussionController.java
@@ -13,12 +13,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
-@RequestMapping("/discussions")
+@RequestMapping("/api/v1/discussions")
 public class DiscussionController {
 
     private final DiscussionService discussionService;
