35c76b6170245a804b529140e759406e8cd348f6
kkm4512 <nayoun440@naver.com>
2024-11-12 15:23:15 +0900

    devOps. dev 브랜치 푸쉬 테스트1

--- src/main/java/com/sparta/doguin/domain/test/AwsTestController.java
+++ src/main/java/com/sparta/doguin/domain/test/AwsTestController.java
@@ -1,5 +1,7 @@
 package com.sparta.doguin.domain.test;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AwsTestController {
     @GetMapping("/health")
-    public String albTest(){
-        return "OK";
+    public ResponseEntity<String> albTest() {
+        return new ResponseEntity<>("OK", HttpStatus.OK);
     }
 }
