4d60d44cd7b9dc7d63a827030fffd2a274f1ce11
kkm4512 <nayoun440@naver.com>
2024-11-12 14:22:58 +0900

    devOps. dev 브랜치 푸쉬 테스트

--- src/main/java/com/sparta/doguin/domain/test/AwsTestController.java
+++ src/main/java/com/sparta/doguin/domain/test/AwsTestController.java
@@ -3,6 +3,7 @@ package com.sparta.doguin.domain.test;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// TEST
 @RestController
 public class AwsTestController {
     @GetMapping("/health")
