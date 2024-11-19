af9f2259044ea0df0fa5ea2da43f91d4829043bb
kkm4512 <nayoun440@naver.com>
2024-11-19 19:18:08 +0900

    fix. 배포 서버, S3 파일작업 수정

--- src/main/java/com/sparta/doguin/domain/common/globalpath/GlobalPath.java
+++ src/main/java/com/sparta/doguin/domain/common/globalpath/GlobalPath.java
@@ -1,5 +1,5 @@
 package com.sparta.doguin.domain.common.globalpath;
 
 public class GlobalPath {
-    public static final String S3_BASE_URL = "https://doguin.s3.ap-northeast-2.amazonaws.com";
+    public static final String S3_BASE_URL = "https://doguins.s3.ap-northeast-2.amazonaws.com";
 }
